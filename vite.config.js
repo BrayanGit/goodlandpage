a231292c4bfc92a04faf8a5da4fffc70715e5f5a
BrayanGit <brayan.dario@outlook.com>
2024-10-05 18:44:14 -0400

    cambio new

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/goodlandpage/', // El nombre del repositorio va aquí
   server: {
     port:3081,
   },
