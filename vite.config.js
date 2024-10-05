f0fe5e3a1ffce9415d9069fcb67211157401daf4
BrayanGit <brayan.dario@outlook.com>
2024-10-05 19:53:34 -0400

    base

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/goodlandpage/', // El nombre del repositorio va aquí
+  plugins: [react()],
   server: {
     port:3081,
   },
